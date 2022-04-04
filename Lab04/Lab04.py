fc4249be0b8b77f548150992a0d38e67fa9d560c
LAPTOP-PA20JO44\Adam <kubiakadam64@gmail.com>
2022-04-04 20:06:28 +0200

    Lab04 Add

---
+++ Lab04/Lab04.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+amount = 150
+sigma = 2,5
+mu = 3
+X = np.random.randn(1,amount)
+
+e1 = np.sum(X)/amount
+
+e2 = np.sum(np.power(X-e1,2))
+
+e3 = np.sum(np.power(X-e1,2))/(amount-1)
+print(e1)
+print(e2)
+print(e3)
