0f1b97ff54f86759d3c367b8f88126f790159f54
LAPTOP-PA20JO44\Adam <249480@student.pwr.edu.pl>
2022-04-05 19:58:50 +0200

    finish

--- Lab03/Lab03_1/Lab03_1.py
+++ Lab03/Lab03_1/Lab03_1.py
@@ -12,3 +12,21 @@ e3 = np.sum(np.power(X-e1,2))/(amount-1)
 print(e1)
 print(e2)
 print(e3)
+
+Y = []
+for x in range(amount):
+    temp = mu + sigma * np.random.randn(1, x)
+    y = np.sum(temp) / (x+1)
+    Y.append(y)
+
+Y1 = []
+for x in range(amount):
+    temp = mu + sigma * np.random.randn(1, x)
+    e1 = np.sum(temp)/(x+1)
+    y = np.sum(np.power(temp-e1,2))
+    Y1.append(y)
+
+x = range(amount)
+plt.plot(x,Y1)
+plt.title("σ = 2.5, μ = 3")
+plt.show()
