10088c976675f55f047842d409f94cd13f4f609b
LAPTOP-PA20JO44\Adam <249480@student.pwr.edu.pl>
2022-04-05 12:03:30 +0200

    fix

--- Lab03/Lab03_1/Lab03_1.py
+++ Lab03/Lab03_1/Lab03_1.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 
 
 amount = 10000
-sigma = 2,5
+sigma = 2.5
 mu = 3
-X = np.random.randn(1,amount)
+X = mu + sigma * np.random.randn(1, amount)
 e1 = np.sum(X)/amount
 e2 = np.sum(np.power(X-e1,2))
 e3 = np.sum(np.power(X-e1,2))/(amount-1)
